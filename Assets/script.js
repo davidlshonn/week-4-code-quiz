e2478abd0cb2311f72b8a24def671f36a31c6afb
David Shonn <davidlshonn@icloud.com>
2020-11-14 20:08:13 +0000

    errors in code fixed

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,64 @@
+var questions = [
+  {
+    question: "What is 2 + 2?",
+    answers: ["4", "22", "5", "2"],
+    correctAnswer: "4",
+  },
+  {
+    question: "What is 5 + 2?",
+    answers: ["4", "22", "7", "2"],
+    correctAnswer: "7",
+  }
+];
+
+var startButton = document.getElementById("start-btn");
+var starterScreen = document.getElementById("starterCode");
+var nextButton = document.getElementById("next-btn");
+var questionContainerElement = document.getElementById("question-container");
+questionContainerElement.style.display = "none";
+var questionElement = document.getElementById("questionAsked");
+var answerButtonsElement = document.getElementById("answer-buttons");
+
+var currentQuestionIndex = 0;
+
+startButton.addEventListener("click", startGame);
+
 function startGame() {
-    
+  starterScreen.style.display = "none";
+  questionContainerElement.style.display = "block";
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  showQuestion(questions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.textContent = question.question;
+  var answersArea = document.getElementById("answer-buttons");
+  answersArea.innerHTML = "";
+  for (var i = 0; i < question.answers.length; i++) {
+    var answerButton = document.createElement("button");
+    answerButton.setAttribute("value", question.answers[i])
+    answerButton.textContent = question.answers[i];
+    answerButton.addEventListener("click", selectAnswer);
+    answersArea.appendChild(answerButton);
+  }
+}
+
+function selectAnswer() {
+    if (this.value === questions[currentQuestionIndex].correctAnswer){
+        alert("fsoinf")
+    } else {
+        // remove time from timer 
+    }
+    currentQuestionIndex++;
+
+    var amountOfQuestions = questions.length; 
+    if (amountOfQuestions === currentQuestionIndex){
+
+    }else {
+
+        showQuestion(questions[currentQuestionIndex])
+    }
 }
